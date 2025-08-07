46d28c98ac1e736b8770c9f920bee804212f5a68
Aryav Khadka <aravxetteri07@gmail.com>
2025-08-07 23:01:24 +0545

    python test

---
+++ test2.py
@@ -0,0 +1,23 @@
+# a = 5
+# print(type(a))
+# print(type(type(a)))
+
+# t1 = (1,2,3,4,5)
+# v = ('a','m','u','l','i','e','o','p','q','w')
+# for i in v:
+#     if i in ['a','e', 'i', 'o','u']:
+#         print(v.index(i))
+# d1 = {1:[1,2,3,4,5],2:[4,5,6]}
+# dictionaryValue =d1.values()
+# # print(dictionaryValue[0])
+# disvalueslist = list(dictionaryValue)
+# # print(disvalueslist[0])
+# d2 = d1.items()
+# print(d2)
+# s1 = {1,2,3,4,5}
+# s1.add(3)
+# print(s1)
+a = [1,2,3]
+b = a.copy()
+b[1]=3
+print(a)
